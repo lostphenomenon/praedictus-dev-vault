b3a65e9cd595c35bb4c16301d3556b0cdf9e38ef
unknown <praedictus@gmail.com>
2018-11-16 00:14:47 +0100

    some more JS vanilla notes

--- Notes/Javascript/Vanilla/4 - Type coersion.js
+++ Notes/Javascript/Vanilla/4 - Type coersion.js
@@ -5,5 +5,7 @@ const val1 = String('5');
 const val2 = 6;
 const sum = val1 + val2;
 
+// sum will be converted to a string
+
 console.log(sum); // 56
 console.log(typeof sum); // string
---
+++ Notes/Javascript/Vanilla/5 - Basic math in JS.js
@@ -0,0 +1,38 @@
+const num1 = 100;
+const num2 = 50;
+
+// Simple math with numbers
+val = num1 + num2;
+val = num1 * num2;
+val = num1 - num2;
+val = num1 / num2;
+val = num1 % num2;
+
+// Using Math object
+val = Math.PI;
+val = Math.E;
+val = Math.sqrt(9);
+val = Math.round(2.4);
+
+// When you want to specify if you want to round up or down
+// up
+val = Math.ceil(2.4);
+// down
+val = Math.floor(2.4);
+
+// abs number
+val = Math.abs(-10);
+
+// power 8 to the secondth
+val = Math.pow(8,2);
+
+// min and max, these take in comme separated values
+val = Math.min(10,9,8,7,-1);
+val = Math.max(10,9,8,7,-1);
+
+// random values can be generated aswell
+// this generated a random float like 0.8491231
+val = Math.random();
+
+// if you want a whole number from one to twenty
+val = Math.floor(Math.random() * 20 + 1);
